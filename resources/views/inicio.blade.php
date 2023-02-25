50956ada39feba38dbd227dfdd81e1ce4ae37393
matheus <matheus20020101@gmail.com>
2023-02-24 22:20:43 -0300

    alteracão

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -8,16 +8,44 @@
         <title>Inicio</title>
         <script>
             $(function(){
-                
+
+                /*Pega info da area de texto de nome campo*/
+                $('#nome_btn').on("click", function(){
+                    let nome_var = $('#campo').val();
+
+                    alert("Olá " + nome_var);
+                })
+
+                /*muda as cores dos h1*/
+                $("#btnRed").on("click", function(){
+                    $("h1").css("color","red");
+                })
+                $("#btnBlack").on("click", function(){
+                    $("h1").css("color","black");
+                })
+                $("#btnGreen").on("click", function(){
+                    $("h1").css("color","green");
+                })
+
             })
 
         </script>
     </head>
 
+
     <body>
         <h1>Eu sou um titulo</h1>
-        
-        <p>esse paragrafo deve ser ocultado</p>
+
+        <p>Pegar info da area de texto</p>
+        <input type="text" id="campo">
+        <button id="nome_btn">Aperte</button>
+        <br>
+        <br>
+        <br>
+        <p>Mudar cores dos Titulos</p>
+        <button id="btnRed">Vermelho</button>
+        <button id="btnBlack">Preto</button>
+        <button id="btnGreen">Verde</button>
 
     </body>
 </html>
