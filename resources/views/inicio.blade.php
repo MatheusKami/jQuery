4a510e800466f26ff23f0366e8398e91d3dfaedc
matheus <matheus20020101@gmail.com>
2023-02-24 21:49:00 -0300

    alteracao

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -6,8 +6,18 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <title>Inicio</title>
+        <script>
+            $(function(){
+                
+            })
+
+        </script>
     </head>
+
     <body>
+        <h1>Eu sou um titulo</h1>
+        
+        <p>esse paragrafo deve ser ocultado</p>
 
     </body>
 </html>
